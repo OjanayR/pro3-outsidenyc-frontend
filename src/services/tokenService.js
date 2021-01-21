a338b81748c337f6a97766760dc453a727e600f3
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-21 14:48:12 -0500

    wip

---
+++ src/services/tokenService.js
@@ -0,0 +1,22 @@
+function setToken() {
+    
+}
+
+function removeToken() {
+    
+}
+
+function getToken() {
+    
+}
+
+function getUserFromToken() {
+    
+}
+
+export {
+    setToken,
+    removeToken,
+    getToken,
+    getUserFromToken
+}
---
+++ src/services/userService.js
@@ -0,0 +1,33 @@
+const BASE_URL = "http://localhost:3001/api/users"
+
+function signup(user) {
+    return fetch(BASE_URL + '/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'Application/json'
+        },
+        body: JSON.stringify(user)
+    }).then(response => {
+        if(response.ok) return response.json();
+        throw new Error ('Email already taken');
+    }).then(({token}) =>setToken (token));
+}
+
+function login {
+    
+}
+
+function logout() {
+    
+}
+
+function getUser() {
+    
+}
+
+export {
+    signup,
+    login,
+    logout,
+    getUser
+}
