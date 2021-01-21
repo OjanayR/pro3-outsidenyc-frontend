1899e28b428feb57b7bd7f0c8c531dc9734556c1
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-21 13:14:55 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 import './App.css';
 
 function App() {
---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,9 @@
+export default function DashboardPage(props) {
+    return (
+        <main className="Page">
+            <h1>
+                DashboardPage
+            </h1>
+        </main>
+    )
+}
